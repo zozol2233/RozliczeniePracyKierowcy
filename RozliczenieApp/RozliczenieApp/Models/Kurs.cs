d9d52879da0953fa0c95c7e6dd8fe76a6405b8d1
MatSen93 <164647133+MatSen93@users.noreply.github.com>
2024-12-21 16:02:25 +0100

    Add files via upload

---
+++ RozliczenieApp/RozliczenieApp/Models/Kurs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RozliczenieApp.Models
+{
+    public class Kurs
+    {
+        public DateTime Data { get; set; } // Data kursu
+        public string Opis { get; set; }   // Opis lokalizacji
+        public int LiczbaKursow { get; set; } // Liczba kursów
+        public decimal Kwota { get; set; } // Zarobiona kwota
+    }
+}
