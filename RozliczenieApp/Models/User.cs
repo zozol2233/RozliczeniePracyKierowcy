f44dc2c3835cde03c75207b17cdba0187dc77722
MatSen93 <164647133+MatSen93@users.noreply.github.com>
2024-12-22 11:48:29 +0100

    Add files via upload

---
+++ RozliczenieApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RozliczenieApp.Models
+{
+    namespace RozliczenieApp.Models
+    {
+        public class User
+        {
+            public string Nazwa { get; set; } // login
+            public string Haslo { get; set; } // haslo
+
+            public virtual string PobierzRole()
+            {
+                return "Użytkownik";
+            }
+        }
+    }
+}
