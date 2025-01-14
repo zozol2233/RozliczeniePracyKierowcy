b8384a3d1f4d6db85d957eeb94182391f2bed691
MatSen93 <164647133+MatSen93@users.noreply.github.com>
2025-01-14 23:51:44 +0100

    Add files via upload
    poprawione klasy z dziedziczeniem user -> driver

---
+++ Kurs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RozliczenieApp.Models
+{
+    public class Kurs
+    {
+        public int Id { get; set; } // Identyfikator kursu
+        public DateTime Data { get; set; } // Data kursu
+        public string Opis { get; set; }   // Opis lokalizacji
+        public int LiczbaKursow { get; set; } // Liczba kursów
+        public decimal Kwota { get; set; } // Zarobiona kwota
+    }
+}
